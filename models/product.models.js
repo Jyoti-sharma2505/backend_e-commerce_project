22cfcbde72f46d73983c069008755a50cc2d1fc3
Jyoti-sharma <jyo.sharma2505@gmail.com>
2025-09-05 13:29:07 +0530

    fixed

--- models/product.models.js
+++ models/product.models.js
@@ -10,9 +10,13 @@ const productSchema = new mongoose.Schema(
     description: String,
     image: String,
     stock: { type: Number, default: 10 },
+
+    // ✅ New fields
+    inCart: { type: Boolean, default: false }, // By default product cart me nahi hoga
+    discount: { type: Number, default: 20 }    // Default 20% discount
   },
   { timestamps: true }
 );
 
-const Product = mongoose.model("Product", productSchema)
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
